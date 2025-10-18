8f6f3895338038a59cc20404c317de2c90586904
shong9124 <shifty393@gmail.com>
2025-10-18 13:03:54 +0900

    [sign-up] 회원가입 예외처리

--- presentation/src/main/java/com/capstone2/presentation/view/sign/SignUpFragment.kt
+++ presentation/src/main/java/com/capstone2/presentation/view/sign/SignUpFragment.kt
@@ -1,8 +1,11 @@
 package com.capstone2.presentation.view.sign
 
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.capstone2.navigation.NavigationCommand
 import com.capstone2.navigation.NavigationRoutes
+import com.capstone2.presentation.R
 import com.capstone2.presentation.base.BaseFragment
 import com.capstone2.presentation.databinding.FragmentSignUpBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,11 +15,50 @@ import kotlinx.coroutines.launch
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
     override fun initView() {
         binding.btnSubmit.setOnClickListener {
+            var allFilled = true
+
+            val etName = binding.etName
+            val etEmail = binding.etEnterEmail
+            val etPw = binding.etEnterPw
+
+            val editTextList = listOf(etName, etEmail, etPw)
+
+            for (editText in editTextList) {
+                val text = editText.text.toString().trim()
+
+                if (text.isEmpty()) {
+                    editText.background = ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.shape_edit_text_type_stroke_error
+                    )
+                    allFilled = false
+                } else if (editText == etEmail && !android.util.Patterns.EMAIL_ADDRESS.matcher(text)
+                        .matches()
+                ) {
+                    editText.background = ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.shape_edit_text_type_stroke_error
+                    )
+                    Toast.makeText(requireContext(), "이메일 형식이 올바르지 않습니다", Toast.LENGTH_SHORT).show()
+                    allFilled = false
+                } else {
+                    editText.background = ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.shape_edit_text_type_stroke
+                    )
+                }
+            }
+            if (!allFilled) {
+                if (etName.text.isNullOrBlank() || etPw.text.isNullOrBlank() || etEmail.text.isNullOrBlank()) {
+                    Toast.makeText(requireContext(), "빈칸을 모두 채워주세요", Toast.LENGTH_SHORT).show()
+                }
+                return@setOnClickListener
+            }
             moveToNext(NavigationRoutes.SignUpComplete)
         }
     }
 
-    private fun moveToNext(route: NavigationRoutes){
+    private fun moveToNext(route: NavigationRoutes) {
         lifecycleScope.launch {
             navigationManager.navigate(
                 NavigationCommand.ToRouteAndClear(route)
